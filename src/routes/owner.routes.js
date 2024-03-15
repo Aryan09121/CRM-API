beaded890d6fcff2363efef79b4e0cf271a36dac
Aryan9901 <911aaryan@gmail.com>
2024-03-15 12:46:04 +0530

    fixed owner.routes.js updateOwnerDetails import

--- src/routes/owner.routes.js
+++ src/routes/owner.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getOwnerById, addNewOwner, getOwners, onwerAvatar } = require("../controllers/owner.controller.js");
+const { getOwnerById, addNewOwner, getOwners, onwerAvatar, updateOwnerDetails } = require("../controllers/owner.controller.js");
 const { authUser } = require("../middlewares/auth.middleware.js");
 const multer = require("multer");
 const { ApiError } = require("../utils/ApiError.js");
@@ -22,7 +22,6 @@ const fileFilter = (req, file, cb) => {
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-
 		cb(null, __dirname + "../../../public/uploads");
 	},
 	filename: function (req, file, cb) {
@@ -38,7 +37,6 @@ router.route("/owner").get(authUser, getOwnerById);
 router.route("/add/owner").post(authUser, addNewOwner);
 // router.route("/add/owner/avatar").Upload.single('avatar').post(authUser, onwerAvatar);
 router.post("/add/owner/avatar", upload.single("avatar"), authUser, onwerAvatar);
-
 
 router.route("/update/owner").patch(authUser, updateOwnerDetails);
 
