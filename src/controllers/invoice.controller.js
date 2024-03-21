8df914a6d6df2efc7923327e865a9230732abcf4
Aryan9901 <911aaryan@gmail.com>
2024-03-21 16:27:06 +0530

    generate invoice added

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -11,15 +11,45 @@ exports.generateInvoice = catchAsyncErrors(async (req, res) => {
 		throw new ApiError(400, "All fields are required");
 	}
 
-	const invoice = await Invoice.create(owner, date, rental, maintainance);
+	// Parse invoice date
+	const invDate = parseDate(date);
+
+	// Parse rental period dates
+	const parsedRental = rental.map((item) => ({
+		...item,
+		period: {
+			from: parseDate(item.period.from),
+			to: parseDate(item.period.to),
+		},
+	}));
+
+	// Create invoice
+	const invoice = await Invoice.create({ owner, date: invDate, rental: parsedRental, maintainance });
 
 	if (!invoice) {
-		throw new ApiError(404, "Trip Not Found");
+		throw new ApiError(404, "Invoice creation failed");
 	}
 
-	console.log(invoice);
+	const inv = await Invoice.findById(invoice._id)
+		.populate({
+			path: "owner",
+			model: "owner", // Assuming the model name for owner is 'Owner'
+		})
+		.populate({
+			path: "rental.cars",
+			model: "Car", // Assuming the model name for cars is 'Car'
+		});
+	if (!inv) {
+		throw new ApiError(404, "Invoice not found");
+	}
 
-	console.log(owner, date, rental, maintainance);
+	console.log(inv);
 
 	// return res.status(200).json(new ApiResponse(200, trip, "Trip Assigned Successfully"));
 });
+
+const parseDate = (dateString) => {
+	const parts = dateString.split("/");
+	// Parts[2] contains year, parts[1] contains month, parts[0] contains day
+	return new Date(parts[2], parts[1] - 1, parts[0]); // Month is zero-based
+};
