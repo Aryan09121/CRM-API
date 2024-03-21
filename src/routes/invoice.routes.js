b12f96d0340b8610479dd6ecc9bec59ea3a05064
Aryan9901 <911aaryan@gmail.com>
2024-03-21 15:16:25 +0530

    invoice routes defined for genreating invoice

--- src/routes/invoice.routes.js
+++ src/routes/invoice.routes.js
@@ -1,5 +1,9 @@
-// ?? ALL INVOICES
-// ?? PENDING
-// ?? PAID INVOICES
-// ?? BILLS
-// ?? CAR REF
+const { Router } = require("express");
+const { generateInvoice } = require("../controllers/invoice.controller");
+
+const router = Router();
+
+router.route("/generate/invoice").post(generateInvoice);
+// router.route("/trip/completed").post(markTripasCompleted);
+
+module.exports = router;
