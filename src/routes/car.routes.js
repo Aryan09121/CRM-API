571967091032a2224c6b25cc4da0419f41627ae7
Aryan9901 <911aaryan@gmail.com>
2024-03-26 15:46:56 +0530

    invoice generation completed

--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,12 +1,20 @@
 const { Router } = require("express");
 const { authUser } = require("../middlewares/auth.middleware.js");
 
-const { getCars, carMaintenance, getSingleCar, getCarsByownerId, getBrandCarBrandsByOwnerId } = require("../controllers/car.controller.js");
+const {
+	getCars,
+	carMaintenance,
+	getSingleCar,
+	getCarsByownerId,
+	getBrandCarBrandsByOwnerId,
+	editCarRate,
+} = require("../controllers/car.controller.js");
 
 const router = Router();
 
 router.route("/cars").get(authUser, getCars);
-router.route("/update/car/maintenance").get(authUser, carMaintenance);
+router.route("/update/car/rate").patch(authUser, editCarRate);
+// router.route("/update/car/maintenance").get(authUser, carMaintenance);
 router.route("/singlecar").get(authUser, getSingleCar);
 router.route("/get/cars/:id").get(authUser, getCarsByownerId);
 router.route("/get/car/brand").get(authUser, getBrandCarBrandsByOwnerId);
