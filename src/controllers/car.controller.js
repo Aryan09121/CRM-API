a0b12d78c83e3a601e95500a010e4a720ba9475a
Aryan9901 <911aaryan@gmail.com>
2024-05-19 13:53:39 +0530

    logs added

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -7,6 +7,7 @@ const { catchAsyncErrors } = require("../middlewares/catchAsyncErrors.js");
 exports.addCar = catchAsyncErrors(async (req, res) => {
 	const { brand, model, registrationNo, rent, start } = req.body;
 
+	console.log(brand, model, registrationNo, rent, start);
 	if (!brand || !model || !registrationNo || !rent || !start.date || !start.km) {
 		throw new ApiError(400, "All fields are required");
 	}
